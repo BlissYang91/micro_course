a0c363c991224950bd3ac71b3c10981b179cc7e5
BlissYang91 <2812420513@qq.com>
2019-12-24 16:50:53 +0800

    学习卡片制作，圆角矩形

--- lib/widgets/ItemMyCourseList.dart
+++ lib/widgets/ItemMyCourseList.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:micro_course/model/MyCourseBean.dart';
 import 'package:micro_course/utils/HexColor.dart';
@@ -28,54 +29,67 @@ class ItemMyCourseListState extends State<ItemMyCourseList> {
           child: Padding(
             padding: EdgeInsets.fromLTRB(15,15,15,0),
           ///圆角卡片布局
-            child: Container(
-              height: 232,
-//              margin: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(15.0)),
-//                border: Border.all(width: 1,color: Colors.redAccent),
-              ),
-              child: Column(
-                children: <Widget>[
-                  Stack(
-                    alignment: AlignmentDirectional.center,
-                    children: <Widget>[
-                      Image.asset(item.imgUrl,fit: BoxFit.fill,height: 143,),
-                      Text(
-                        item.grade+'新',
-                        style: TextStyle(fontSize: 15,color: Colors.white),
-                      ),
-                    ],
-                  ),
-                  Padding(padding: EdgeInsets.fromLTRB(7.5, 0, 7.5, 0),child:  Container(
-                    height: 89,
-                    color: Colors.white,
-                    child: Stack(
+            child: SizedBox(
+              height: 240,
+              child: Card(
+                shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                child: Column(
+                  children: <Widget>[
+                    Stack(
                       alignment: AlignmentDirectional.center,
                       children: <Widget>[
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-//                          borderRadius: BorderRadius.all(Radius.circular(45)),
-                          ),
-                        ),
-                        Container(
-                          height: 50,
-                          width: 325,
-                          decoration: BoxDecoration(
-                            color: Colors.yellow[800],
-                            borderRadius: BorderRadius.all(Radius.circular(45)),
-                          ),
-                        ),
+//                        Image.asset(item.imgUrl,fit: BoxFit.fill,height: 143),
+                       Container(
+                         height: 140,
+                         width: 580,
+                         decoration: BoxDecoration(
+                           image: DecorationImage(
+                             image: AssetImage(item.imgUrl),
+                             fit: BoxFit.fitWidth,
+                           ),
+                           borderRadius: BorderRadius.vertical(
+                             ///顶部圆角
+                             top: Radius.circular(10.0),
+                           ),
+                         ),
+                       ),
                         Text(
-                          '开始学习',
-                          style: TextStyle(fontSize: 18,color: HexColor('#824E00')),
+                          item.grade+'新',
+
                         ),
+//                        Image.asset("images/distribution/studing.png"),
                       ],
                     ),
-                  ), )
+                    Padding(padding: EdgeInsets.fromLTRB(7.5, 0, 7.5, 0),child:  Container(
+                      height: 89,
+                      color: Colors.white,
+                      child: Stack(
+                        alignment: AlignmentDirectional.center,
+                        children: <Widget>[
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+//                          borderRadius: BorderRadius.all(Radius.circular(45)),
+                            ),
+                          ),
+                          Container(
+                            height: 50,
+                            width: 325,
+                            decoration: BoxDecoration(
+                              color: Colors.yellow[800],
+                              borderRadius: BorderRadius.all(Radius.circular(45)),
+                            ),
+                          ),
+                          Text(
+                            '开始学习',
+                            style: TextStyle(fontSize: 18,color: HexColor('#824E00')),
+                          ),
+                        ],
+                      ),
+                    ), )
 
-                ],
+                  ],
+                ),
               ),
             ),
           )),
